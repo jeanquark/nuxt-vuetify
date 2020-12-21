376a36f78b8c81c6ff2b9e6d85e17e3fa7c8e4c9
Jean-Marc <jm.kleger@gmail.com>
2020-12-21 21:58:41 +0100

    Initial commit

---
+++ vuetify.options.js
@@ -0,0 +1,26 @@
+export default {
+    theme: {
+        dark: true,
+        themes: {
+            light: {
+                primary: '7A528F', // light purple
+                accent: 'E0E0E0', // grey lighten-1
+                secondary: '#FFC107', // amber
+                tertiary: '#E11566', // pink
+                dark: '#7e7e7e', // dark-grey
+                background: '#ffffff' // white,
+            },
+            dark: {
+                primary: '7A528F', // light purple
+                accent: 'E0E0E0', // grey lighten-1
+                secondary: '#FFC107', // amber
+                tertiary: '#E11566', // pink,
+                dark: '#7e7e7e', // dark-grey
+                background: '#424242' // dark
+            }
+        },
+        options: {
+            customProperties: true
+        }
+    }
+};
